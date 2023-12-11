f334f40cf89f8c66b8b205767379ee1285303f8a
PavelRepnikov <rtfiof@gmail.com>
2023-12-11 18:33:47 +0300

    commit

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ sns.histplot(data[selected_feature], kde=True)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
 
-columns_to_drop = ['EDUCATION', 'MARITAL_STATUS', 'GEN_INDUSTRY', 'GEN_TITLE', 'JOB_DIR', 'FAMILY_INCOME']
-data_corr = data.drop(columns=columns_to_drop)
-st.subheader("Correlation Matrix")
-corr_matrix = data.corr()
-sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f")
-st.pyplot()
+#columns_to_drop = ['EDUCATION', 'MARITAL_STATUS', 'GEN_INDUSTRY', 'GEN_TITLE', 'JOB_DIR', 'FAMILY_INCOME']
+#data_corr = data.drop(columns=columns_to_drop)
+#st.subheader("Correlation Matrix")
+#corr_matrix = data.corr()
+#sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+#st.pyplot()
 
 st.subheader("Target Variable vs Features")
 target_variable = st.selectbox("Select the target variable", data_corr.columns)
