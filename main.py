75952ed99bfeaa83da363efae3d881310f14dc43
PavelRepnikov <rtfiof@gmail.com>
2023-12-11 18:19:06 +0300

    commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data_url = "./hw_1_data.csv"
+data_url = "https://github.com/PavelRepnikov/hw_1_applied_python/blob/main/hw_1_data.csv"
 data = pd.read_csv(data_url)
 
 st.title("Exploratory Data Analysis with Streamlit")
@@ -21,22 +21,25 @@ st.text(data.columns.tolist())
 st.subheader("Feature Distributions")
 selected_feature = st.selectbox("Select a feature", data.columns)
 sns.histplot(data[selected_feature], kde=True)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
 
+columns_to_drop = ['EDUCATION', 'MARITAL_STATUS', 'GEN_INDUSTRY', 'GEN_TITLE', 'JOB_DIR', 'FAMILY_INCOME']
+data_corr = data.drop(columns=columns_to_drop)
 st.subheader("Correlation Matrix")
 corr_matrix = data.corr()
 sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f")
 st.pyplot()
 
 st.subheader("Target Variable vs Features")
-target_variable = st.selectbox("Select the target variable", data.columns)
+target_variable = st.selectbox("Select the target variable", data_corr.columns)
 sns.scatterplot(x=data[selected_feature], y=data[target_variable])
 plt.xlabel(selected_feature)
 plt.ylabel(target_variable)
 st.pyplot()
 
 st.subheader("Numerical Characteristics")
-numerical_summary = data.describe()
+numerical_summary = data_corr.describe()
 st.dataframe(numerical_summary)
 
 if __name__ == "__main__":
